7ebdd7057328e84938cfdd82b713916961624dd5
abiTolentino <josephreytolentino@gmail.com>
2022-07-28 13:27:22 +0800

    sjhc

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,86 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <button @click="compare">cancel</button>
+    <button @click="setObjCookie">Set Object to Cookie</button>
+    <button @click="getCookie">Get Cookie Content</button>
   </div>
 </template>
 
-<script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+<script>
 
-@Component
-export default class HelloWorld extends Vue {
-  @Prop() private msg!: string;
+//    function setCookie(name,value) {
+//     document.cookie = name + "=" + (value || "")  + "; path=/";
+// }
+ 
+// function delete_cookie(name) {
+//   document.cookie = [name, '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain=.', window.location.host.toString()].join('');
+// }
+
+var person = { name:'Rudssss', age: 10, color:'black'}; //mock up object in local
+var returnItem = getCookie("personDetails"); // specifies the object in cookie
+
+function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1,c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    }
+    return null;
 }
+
+
+export default {
+  // data() {
+  //   return {
+  //         person : { name:'joe', age: 7, color:'green'}
+  //     }
+  //   },
+  methods: {
+    compare() {
+        console.log(JSON.stringify(person) + "---person from local");
+        console.log(returnItem + "---person from cookie");
+
+        if(JSON.stringify(person) === returnItem) {
+          console.log("no changes, DON'T show modal");
+        } else {
+          console.log("changes made, SHOW modal");
+        }
+    },
+
+    setObjCookie() {
+      let a = JSON.stringify(person);
+      this.setCookie("personDetails", a);
+      console.log(a);
+    },
+
+    setCookie(name,value) {
+        document.cookie = name + "=" + (value || "")  + "; path=/";
+    },
+
+    // getCookie(name) {
+    //     var nameEQ = name + "=";
+    //     var ca = document.cookie.split(';');
+    //     for(var i=0;i < ca.length;i++) {
+    //         var c = ca[i];
+    //         while (c.charAt(0)==' ') c = c.substring(1,c.length);
+    //         if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+    //     }
+    //     return null;
+    // },
+    
+    getCookie() {
+        console.log(document.cookie);
+    },
+
+    delete_cookie(name) {
+      document.cookie = [name, '=; expires=Thu, 01-Jan-1970 00:00:01 GMT; path=/; domain=.', window.location.host.toString()].join('');
+    }
+    
+
+  }
+}
+
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
